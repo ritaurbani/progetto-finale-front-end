a332e29246e52e63ab75ab07a14d747b1792f553
ritaurbani <ritaurbani@gmail.com>
2025-05-29 12:00:58 +0200

    c

--- src/Components/SearchBar.tsx
+++ src/Components/SearchBar.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { useSearchParams } from "react-router-dom"
+// import { useSearchParams } from "react-router-dom"
 
 
 
@@ -8,7 +8,7 @@ type SearchBarPprops = {
   onChangeText: (value: string) => void,
 }
 
-const SearchBar = ({ onChangeText}: SearchBarPprops) => {
+const SearchBar = ({ onChangeText }: SearchBarPprops) => {
 
   const [searchValue, setSearchValue] = useState("")
 
@@ -20,17 +20,21 @@ const SearchBar = ({ onChangeText}: SearchBarPprops) => {
 
   return (
     <>
-      <label htmlFor=""></label>
-      <input
-        type="text"
-        placeholder="search for a product"
-        value={searchValue}
-        onChange={(e) => {
-        
-          setSearchValue(e.target.value.toLowerCase()) 
-          onChangeText(e.target.value)
-        }
-        } />
+      {/* <div style={{ position: 'relative' }} className="input-icon"> */}
+        <label htmlFor=""></label>
+        <input
+          type="text"
+          placeholder="search for a product"
+          value={searchValue}
+          onChange={(e) => {
+
+            setSearchValue(e.target.value.toLowerCase())
+            onChangeText(e.target.value)
+          }
+          }
+        />
+        {/* <i className="fa-solid fa-magnifying-glass"></i> */}
+      {/* </div> */}
     </>
   )
 }

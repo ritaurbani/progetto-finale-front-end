853c6c98ea01b6a2e1c142d2ee578c00b7443d04
ritaurbani <ritaurbani@gmail.com>
2025-06-02 16:57:17 +0200

    localstorage

--- src/Components/ProgressBar.tsx
+++ src/Components/ProgressBar.tsx
@@ -6,17 +6,13 @@ type ProgressBarProp = {
 export const ProgressBar = ({rate}:ProgressBarProp) => {
 
     // Math.round(rate)
-    const rateNumber = Math.round(rate)
+    // const rateNumber = Math.round(rate)
 
     return (
         <div className='pbar-container'>
             <div className='bar-fill-group'>
-                <div className='progress-bar'
-                role="progressbar"
-                aria-valuenow={rate}
-                aria-valuemin={0}
-                aria-valuemax={10}>
-                    <div className='progress-bar-fill' style={{ width: `${rateNumber}%`, 
+                <div className='progress-bar'>
+                    <div className='progress-bar-fill' style={{ width: `${rate*10}%`, 
                     backgroundColor: "#55394D", transition: "width 0.5s ease-in-out"}}>
                     </div>
                 </div>

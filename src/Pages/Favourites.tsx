06a1b11ec0e36bbaaf0bdf2d14985014b3e86e61
ritaurbani <ritaurbani@gmail.com>
2025-06-05 15:50:58 +0200

    Favs

--- src/Pages/Favourites.tsx
+++ src/Pages/Favourites.tsx
@@ -5,26 +5,23 @@ import BankCard from "../Components/BankCard"
 
 const Favourites = () => {
 
-    const { favourites, setFavourites, addToFavourites, removeFromFavourites } = useContext(GlobalContext)
+    const { favourites, removeFromFavourites } = useContext(GlobalContext)
 
-    const removeItem = (id) => {
-        const removedItem = favourites.filter((item) => item.id !== id)
 
-    }
 
-  return (
-    <div>
-        <h3 style={{margin: "20px"}}>Your Favourites</h3>
-        {
-            favourites.map((item) => (
-                <div className="favourites-card">
-                    <h3>{item.title}</h3>
-                    <span onClick={() => removeItem(item.id)}><i className="fa-solid fa-xmark"></i></span>
-                </div>
-            ))
-        }
-    </div>
-  )
+    return (
+        <div>
+            <h3 style={{ margin: "20px" }}>Your Favourites</h3>
+            {
+                favourites.map((item) => (
+                    <div className="favourites-card">
+                        <h3>{item.title}</h3>
+                        <span onClick={() => removeFromFavourites(item.id)}><strong>X</strong></span>
+                    </div>
+                ))
+            }
+        </div>
+    )
 }
 
 export default Favourites

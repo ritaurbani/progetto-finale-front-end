73c0847495d28d08758db0cbd3872e3c4e65fe25
ritaurbani <ritaurbani@gmail.com>
2025-06-15 17:03:38 +0200

    changes

--- src/CustomHooks/useProducts.ts
+++ src/CustomHooks/useProducts.ts
@@ -28,14 +28,13 @@ export const useProducts = (): UseProductsReturnType => {
         try {
             setIsLoading(true)
             const response = await fetch(`${API_URL}/bankproducts`)
-            if (response.ok) {
-            }
             const data = await response.json()
-
             setBankProducts(data)
         } catch (error: unknown) {//di default e unknown-non puoi usare erro senza prima verificare il suo tipo-forza type checking
             if (error instanceof Error) {
                 setError(error.message)
+            }else{
+                setError('errore sconosciuto')
             }
         } finally {
             setIsLoading(false)
@@ -49,40 +48,7 @@ export const useProducts = (): UseProductsReturnType => {
     //oggetto piu flessibili quando ci sono più valori da ritornare (es. dati, loading, errori).
     //se ritorno array poi devo ricordare ordine elems
     return { products: bankProducts, isLoading, error, fetchProducts  }
-    //products → Nome della proprietà nell'oggetto ritornato (quello che vedrà chi usa il hook).
-    // bankProducts → Variabile interna al hook.
-    // Se hai definito un tipo per il ritorno del hook(es.UseProductsReturnType), devi assicurarti che i nomi delle proprietà coincidano:
-    // type UseProductsReturnType = {
-    //     products: BankProduct[]; // Tipo atteso
-    // };
-
-    // return {
-    //     prodotti: bankProducts // ❌ ERRORE: "prodotti" non è una proprietà del tipo!
-    //   };
+    //uso proprieta
+    
 
 }
-
-// type inline
-//se ritorni solo array no oggetto-const useProducts = (): BankProduct[] 
-// export const useProducts = (): { products: BankProduct[] } => {
-//     const [products, setProducts] = useState<BankProduct[]>([]);
-//     return { products };
-// };
-
-// tipi complessi
-// type UseProductsReturnType = {
-//     products: BankProduct[];
-//     isLoading: boolean;
-//     error: string | null;
-//     refresh: () => void;
-// };
-
-// export const useProducts = (): UseProductsReturnType => {
-//     const [products, setProducts] = useState<BankProduct[]>([]);
-//     const [isLoading, setIsLoading] = useState(false);
-//     const [error, setError] = useState<string | null>(null);
-
-//     const refresh = () => { /* ... */ };
-
-//     return { products, isLoading, error, refresh }; // Ora il tipo separato è utile
-// };
